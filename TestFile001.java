7242800c18702eb4119dcbdc343a36bbe18ee809
724493597 <115340932+724493597@users.noreply.github.com>
2022-11-15 16:59:30 +0800

    Update TestFile001.java

--- TestFile001.java
+++ TestFile001.java
@@ -3,7 +3,7 @@ package cmic.common;
 public class TestFile001 {	
 	
 	public static void main(String[] args) {
-      System.out.println("1980202");
+      		System.out.println("198020214");
 	}
 	
 }
